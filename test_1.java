f682b2223b0d4753640f36fe369832709bd3bd41
coder-50-glitch <dharrajashree74@gmail.com>
2025-08-08 18:08:11 +0530

    Add files via upload

---
+++ test_1.java
@@ -0,0 +1,13 @@
+class test_1 {
+    
+    public static void main(String[] args) {  
+    System.out.println("My Maggie Recipe");
+	Maggie();
+  }
+public static void Maggie() {
+ System.out.println(" boiled water")   ;
+ System.err.println("pore maggie");
+
+}
+      
+  }
