7148deb83410701df6a9bec3f23b1736f049922c
coder-50-glitch <dharrajashree74@gmail.com>
2025-08-24 23:10:05 +0530

    Add files via upload

---
+++ first_file.java
@@ -0,0 +1,17 @@
+
+class first_file{
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+         System.out.println("Additon of a&b: " + sum);
+        int diff = a - b;
+       System.out.println("Subtraction  of a&b:" + diff);
+        int mult = a * b;
+       System.out.println("Multiplication of a&b: " + mult);
+        int div = a / b;        
+        System.out.println("Division  of a&b: " + div);
+        
+    }
+}
